dc84f4564721c0236f8921868683e8b793eedcab
demoweb1 <demoweb1@demoweblink.com>
2022-02-03 10:29:54 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReducerHook from "./components/ReducerHook";
+import StateHook from "./components/StateHook";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import EffectHook from "./components/EffectHook";
+import RefHook from "./components/RefHook";
+import LayoutEffectHook from "./components/LayoutEffectHook";
+import ImperativeHandle from "./components/ImperativeHandle";
+import Context from "./components/Context";
+import Memo from "./components/Memo";
+import Callback from "./components/Callback";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/usestate" element={<StateHook />} />
+        <Route path="/usereducer" element={<ReducerHook />} />
+        <Route path="/useeffect" element={<EffectHook />} />
+        <Route path="/useref" element={<RefHook />} />
+        <Route path="/uselayouteffect" element={<LayoutEffectHook />} />
+        <Route path="/useimperativehandle" element={<ImperativeHandle />} />
+        <Route path="/usecontext" element={<Context />} />
+        <Route path="/usememo" element={<Memo />} />
+        <Route path="/usecallback" element={<Callback />} />
+      </Routes>
+    </Layout>
   );
 }
-
-export default App;
